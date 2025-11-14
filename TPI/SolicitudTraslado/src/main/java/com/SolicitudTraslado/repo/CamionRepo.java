86974ec5c1f4f698320d32bdda6eceffc59d340e
Nicole Chauvet <nicolechauvett@gmail.com>
2025-11-14 10:21:16 -0300

    terminado mis repos

--- TPI/SolicitudTraslado/src/main/java/com/SolicitudTraslado/repo/CamionRepo.java
+++ TPI/SolicitudTraslado/src/main/java/com/SolicitudTraslado/repo/CamionRepo.java
@@ -1,10 +1,8 @@
 package com.SolicitudTraslado.repo;
 
-import com.SolicitudTraslado.domain.Camion;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface CamionRepo {
-    
+
 }
---
+++ TPI/SolicitudTraslado/src/main/java/com/SolicitudTraslado/repo/ContenedorRepo.java
@@ -0,0 +1,20 @@
+package com.SolicitudTraslado.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.SolicitudTraslado.domain.Cliente;
+import com.SolicitudTraslado.domain.Contenedor;
+import com.SolicitudTraslado.domain.enums.EstadoContenedor;
+
+@Repository
+public interface ContenedorRepo extends JpaRepository<Contenedor, Long> {
+    List<Contenedor> findByEstadoContenedor(EstadoContenedor estadoContenedor);
+
+    List<Contenedor> findByClienteId(Long clienteId);
+
+    List<Contenedor> findByCliente(Cliente cliente);
+
+}
