fcadb01349c036d610a52978acde0cbee506cbe9
Ezequiel Di Pietro <94357@sistemas.frc.utn.edu.ar>
2025-11-14 09:42:22 -0300

    Progreso TPI

---
+++ TPI/SolicitudTraslado/src/main/java/com/SolicitudTraslado/repo/CamionRepo.java
@@ -0,0 +1,10 @@
+package com.SolicitudTraslado.repo;
+
+import com.SolicitudTraslado.domain.Camion;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CamionRepo {
+    
+}
